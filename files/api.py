49111e823757e27e7050585e03ffa21d2d181af0
unknown <apowell@umass.edu>
2019-10-08 10:40:01 -0400

    Testing api and flask setup

--- files/api.py
+++ files/api.py
@@ -12,12 +12,26 @@ app = Flask(__name__)
 # )
 # cursor = mydb.cursor(dictionary=True)
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(description='A typical hello world')
+class Food(graphene.ObjectType):
+    name = graphene.String()
+    ingredients = graphene.List(graphene.String)
 
-    def resolve_hello(self, info):
-        return 'World'
+    def resolve_name(parent, info):
+        return "Spaghetti"
+    
+    def resolve_ingredients(parent, info):
+        return ["Peanuts", "Tree Nuts", "Coconut", "Sesame"]
+
+class Query(graphene.ObjectType):
+    # this defines a Field `hello` in our Schema with a single Argument `name`
+    foods = graphene.List(Food)
+
+    def resolve_foods(parent, info):
+        return [Food()]
 
 schema = graphene.Schema(query=Query)
 
 app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True))
+
+if __name__ == "__main__":
+    app.run()
