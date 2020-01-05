ea0a09a80bf97a7d3cfc9fa8e76270343fa311a4
Adam Powell <apowell@umass.edu>
2020-01-04 22:55:16 -0500

    Removed vagrant

--- script.sh
+++
@@ -1,3 +0,0 @@
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password password'
-sudo debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password password'
-sudo apt-get -y install mysql-server
